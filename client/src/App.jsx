fb324222634cdede505ea74ba2a40c3aa147e66c
Farhan Fansuri <farhanfansuri2023@gmail.com>
2023-11-22 14:12:54 +0700

    membuat heaser component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
