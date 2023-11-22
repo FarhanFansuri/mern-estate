01b2acac0a2bedb4ae7bf5b2f8c5b77ab256cf8d
Farhan Fansuri <farhanfansuri2023@gmail.com>
2023-11-22 13:36:38 +0700

    membuat struktur folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 export default function App() {
   return (
     <div>
-      <h1 className='m-4'>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   )
 }
